17c83b36c07c91476d26820e7fe54a1b71e9cd82
Reshmi R S <reshmirs@Reshmis-MacBook-Pro.local>
2023-03-23 18:12:47 +0530

    Add model for department

--- hospital/models.py
+++ hospital/models.py
@@ -2,9 +2,12 @@ from django.db import models
 # Create your models here.
 
 
+class Department(models.Model):
+    name = models.CharField(primary_key=True,max_length=20,unique=True)
+
 class Doctor(models.Model):
     name = models.CharField(max_length=20)
-    department = models.CharField(max_length=20)
+    department = models.ForeignKey(Department,on_delete=models.CASCADE)
     op_start_time = models.TimeField()
     op_end_time = models.TimeField()
     consultation_charge = models.IntegerField()
@@ -17,4 +20,5 @@ class Customer(models.Model):
 class Admin(models.Model):
     user_name = models.CharField(max_length=15)
     password = models.CharField(max_length=10)
-    token=models.CharField(max_length=20,default=None, blank=True, null=True)
+    token = models.CharField(
+        max_length=20, default=None, blank=True, null=True)
